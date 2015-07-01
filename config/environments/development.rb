439d3f0c318bb65ea3dff3c97b9be8656db5a402
Anton <anton.perebyinis@gmail.com>
2015-07-01 16:01:56 +0300

    Add action:mailer configuration for restore password/ Add styling for restore password form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,17 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :authentication => :plain,
+      :domain => 'gmail.com',
+      :user_name => 'username@gmail.com',
+      :password => 'secret'
+  }
 end
