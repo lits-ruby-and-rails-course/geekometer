9af158d63e9daca407671a219c625a6f9750757f
Taras Kunch <taras.kunch@gmail.com>
2015-07-15 20:54:47 +0300

    Fix actionamiler config for production

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -6,3 +6,8 @@ ActionMailer::Base.smtp_settings = {
   :user_name => 'geekometer.development@gmail.com',
   :password  => ENV['SMTP_PASSWORD'] || 'IJ4-RJzgZJdK0I48iOQrnA'
 }
+Rails.application.config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+Rails.application.config.action_mailer.delivery_method = :smtp
+Rails.application.config.action_mailer.perform_deliveries = true
+Rails.application.config.action_mailer.raise_delivery_errors = true
+Rails.application.config.action_mailer.default :charset => "utf-8"
