b6efa1672d76b3e1885eb519be067acbce30e855
scarlet1986 <scarlet1986@ukr.net>
2015-08-06 16:23:01 +0300

    add show page for new guestion

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,14 +7,14 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
-    4.times { @question.answers.build }
+    4.times { @question.answers.build }  
   end
 
   def create
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to root_path, notice: 'Question was successfully created.'
+      redirect_to question_path(@question), notice: 'Question was successfully created.'
     else
       flash.now[:danger] = 'Required fields should be present'
       render :new
@@ -31,5 +31,9 @@ class QuestionsController < ApplicationController
     params.require(:question)
     .permit(:condition, :difficulty, :topic_id, \
             answers_attributes: [:id,:solution, :answer_valid, :_destroy])
+  end
+
+  def show
+    @question = Question.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get 'test' => 'pages#tests'
   get '/users/:id' => 'users#show'
 
-  resources :questions, only: [:index, :new, :create]
+  resources :questions, only: [:index, :new, :create, :show]
   resources :topics, only: [:index, :show, :new, :create]
 
   resources :users, only: [:create, :edit, :new, :show, :testpage, :update] do
