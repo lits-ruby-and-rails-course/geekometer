ef4a4edc52887bdac39266334d30785249a74f94
scarlet1986 <scarlet1986@ukr.net>
2015-08-12 18:08:10 +0300

    add redcarpet and other gems to use code in questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def markdown(text)
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+        return markdown.render(text).html_safe
+      end
+
+    def syntax_highlighter(html)
+      doc = Nokogiri::HTML(html)
+      doc.search("//pre[@lang]").each do |pre|
+        pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+      doc.to_s
+  end
 end
