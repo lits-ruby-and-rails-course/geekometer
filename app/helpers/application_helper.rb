47dc6a60e89b0259e224eec3c3f93d19d2994587
scarlet1986 <scarlet1986@ukr.net>
2015-08-13 23:36:23 +0300

    block with code in questions fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     class CodeRayify < Redcarpet::Render::HTML
       def block_code(code, language)
-        CodeRay.scan(code, language).div(:line_numbers => :table)
+        CodeRay.scan(code, language).div
       end
     end
       def markdown(text)
